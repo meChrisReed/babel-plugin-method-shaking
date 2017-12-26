cb97ea3d26dcb7892aebbda675b7d4f7a3c2a099
Chris Reed <mechrisreed@gmail.com>
2017-12-26 14:14:50 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const merge = require("deep-extend")
-
 // isMethod checks that this ObjectProperty holds a function
 // I tried `t.isObjectMethod` from `babel-types` it did not identify any of the methods with the `ObjectProperty` node type
 // There is likely a better / more Babel way to do this
@@ -17,53 +15,48 @@ const isMethod = ({ node: { type, value } }) =>
   }.true)
 
 // TODO: createMemberExpressionCache contains a mutation
+// caches all of the MemberExpression for comparison
 const createMemberExpressionCache = (cache = []) => ({
   updateCache: data => cache.push(data),
   getCache: () => cache
 })
 
+// (object containing method, method name, cache) -> Boolean
 const isUsedMethod = (obj, method, cache) =>
   cache.find(cached => cached.rawObject === obj && cached.rawMethod === method)
 
-module.exports = (
-  { types: t },
-  memberExpressionCache = createMemberExpressionCache()
-) => ({
-  visitor: {
-    Program: programPath => {
-      programPath.traverse({
-        MemberExpression: memberExpressionPath => {
-          memberExpressionCache.updateCache({
-            rawObject: memberExpressionPath.node.object.name,
-            rawMethod: memberExpressionPath.node.property.name
-          })
-        }
-      })
-    },
-    ObjectProperty: objectPropertyPath => {
-      const name = objectPropertyPath.node.key.name
-      const parentObject = objectPropertyPath.findParent(
-        parentPath => parentPath.type === "VariableDeclarator"
-      )
+module.exports = ({ types: t }) => {
+  const memberExpressionCache = createMemberExpressionCache()
 
-      if (
-        isMethod(objectPropertyPath) &&
-        !isUsedMethod(
-          parentObject.node.id.name,
-          name,
-          memberExpressionCache.getCache()
+  return {
+    visitor: {
+      Program: programPath => {
+        programPath.traverse({
+          MemberExpression: memberExpressionPath => {
+            memberExpressionCache.updateCache({
+              rawObject: memberExpressionPath.node.object.name,
+              rawMethod: memberExpressionPath.node.property.name
+            })
+          }
+        })
+      },
+      ObjectProperty: objectPropertyPath => {
+        const name = objectPropertyPath.node.key.name
+        const parentObject = objectPropertyPath.findParent(
+          parentPath => parentPath.type === "VariableDeclarator"
         )
-      ) {
-        // from here I need to check the entire source to see if this Identifier is used
-        // right now I am not worried about renamed identifiers
-        // So I need to filter the path for every path that reflect this path.
-        // VariableDeclaration -> Identifier .init.properties -> name "unused"
-        //                             |                          |
-        //                             _________________        __
-        //                                              |      |
-        // ExpressionStatement -> MemberExpression -> object property
-        objectPropertyPath.remove()
+
+        if (
+          isMethod(objectPropertyPath) &&
+          !isUsedMethod(
+            parentObject.node.id.name,
+            name,
+            memberExpressionCache.getCache()
+          )
+        ) {
+          objectPropertyPath.remove()
+        }
       }
     }
   }
-})
+}
